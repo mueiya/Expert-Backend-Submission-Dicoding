d48e88cb2675594555bff1cf6840a3418b2e23ac
mueiya <mueiya.whatever@gmail.com>
2023-04-05 14:13:32 +0700

    add migrate for create table comments and replies

---
+++ migrations/1680676976530_create-table-comments.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    thread: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: 'false',
+    },
+  });
+  // Adding foreign key threads(id) as thread
+  pgm.addConstraint('comments', 'fk_comments_threads', 'FOREIGN KEY(thread) REFERENCES threads(id) ON DELETE CASCADE');
+  // Adding foreign key users(id) as owner
+  pgm.addConstraint('comments', 'fk_comments_users', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
---
+++ migrations/1680678043363_create-table-replies.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primarykey: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    comment: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    deleted: {
+      type: 'BOOLEAN',
+      notNull: true,
+      default: 'false',
+    },
+  });
+
+  // Adding foreign key comments(id) as comment
+  pgm.addConstraint('replies', 'fk_replies_comments', 'FOREIGN KEY(comment) REFERENCES comments(id) ON DELETE CASCADE');
+  // Adding foreign key users(id) as owner
+  pgm.addConstraint('replies', 'fk_replies_users', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('replies');
+};
