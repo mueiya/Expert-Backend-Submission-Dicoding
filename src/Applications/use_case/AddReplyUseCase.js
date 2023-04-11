8cfe39e3260cc3ee6cb8be74e8054f0c1d695889
mueiya <mueiya.whatever@gmail.com>
2023-04-11 08:50:31 +0700

    Add reply use case

---
+++ src/Applications/use_case/AddReplyUseCase.js
@@ -0,0 +1,19 @@
+const PostReply = require('../../Domains/replies/entities/PostReply');
+
+class AddReplyUseCase {
+  constructor({thredRepository, commentRepository, replyRepository}) {
+    this._threadRepository = thredRepository;
+    this._commentRepository = commentRepository;
+    this._replyRepository = replyRepository;
+  }
+
+  async execute(useCasePayload) {
+    const {threadId, commentId} = useCasePayload;
+    await this._threadRepository.getThreadById(threadId);
+    await this._commentRepository.getCommentById(commentId);
+    const postReply = new PostReply(useCasePayload);
+    return this._replyRepository.addReply(postReply);
+  }
+}
+
+module.exports = AddReplyUseCase;
---
+++ src/Applications/use_case/_test/AddReplyUseCase.test.js
@@ -0,0 +1,66 @@
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const PostedReply = require('../../../Domains/replies/entities/PostedReply');
+const PostReply = require('../../../Domains/replies/entities/PostReply');
+const ReplyRepository = require('../../../Domains/replies/ReplyRepository');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const AddReplyUseCase = require('../AddReplyUseCase');
+
+describe('AddReplyUseCase', () => {
+  /**
+  * to test wheter the use case can consentrate step by step correctly
+  */
+  it('should orchestrating the add action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      content: 'reply content aja',
+      threadId: 'thread-1234',
+      commentId: 'comment-1345',
+      owner: 'user-4353',
+    };
+
+    const mockPostedReply = new PostedReply({
+      id: 'reply-1234',
+      content: useCasePayload.content,
+      owner: useCasePayload.owner,
+    });
+
+    /** Creating dependency of use case */
+    const mockReplyRepository = new ReplyRepository();
+    const mockCommentRepository = new CommentRepository();
+    const mockThreadRepository = new ThreadRepository();
+
+    /** Mocking needed function */
+    mockThreadRepository.getThreadById = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.getCommentById = jest.fn()
+        .mockImplementation(() => Promise.resolve());
+    mockReplyRepository.addReply = jest.fn()
+        .mockImplementation(() => Promise.resolve(mockPostedReply));
+
+    /** Creating use case instance */
+    const addReplyUseCase = new AddReplyUseCase({
+      thredRepository: mockThreadRepository,
+      commentRepository: mockCommentRepository,
+      replyRepository: mockReplyRepository,
+    });
+
+    // Action
+    const postedReply = await addReplyUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(postedReply).toStrictEqual(new PostedReply({
+      id: 'reply-1234',
+      content: useCasePayload.content,
+      owner: useCasePayload.owner,
+    }));
+
+    expect(mockThreadRepository.getThreadById).toBeCalledWith(useCasePayload.threadId);
+    expect(mockCommentRepository.getCommentById).toBeCalledWith(useCasePayload.commentId);
+    expect(mockReplyRepository.addReply).toBeCalledWith(new PostReply({
+      content: useCasePayload.content,
+      threadId: useCasePayload.threadId,
+      commentId: useCasePayload.commentId,
+      owner: useCasePayload.owner,
+    }));
+  });
+});
