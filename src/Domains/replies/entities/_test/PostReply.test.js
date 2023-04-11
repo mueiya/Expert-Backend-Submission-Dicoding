08df32ed14e3d68868a0f02f0fe902632d286cdd
mueiya <mueiya.whatever@gmail.com>
2023-04-11 08:51:11 +0700

    adding threadId and commentId as payload property

--- src/Domains/replies/entities/_test/PostReply.test.js
+++ src/Domains/replies/entities/_test/PostReply.test.js
@@ -4,7 +4,8 @@ describe('a PostReply entities', () => {
   it('should throw error when payload did not contain needed property', () => {
     // Arrange
     const payload = {
-
+      content: 'lah lah lah!!',
+      threadId: 'thread-1234',
     };
 
     // Action and Assert
@@ -15,6 +16,9 @@ describe('a PostReply entities', () => {
     // Arrange
     const payload = {
       content: 123,
+      threadId: [],
+      commentId: 234,
+      owner: {},
     };
 
     // Action and Assert
@@ -25,6 +29,9 @@ describe('a PostReply entities', () => {
     // Arrange
     const payload = {
       content: 'Sama ane juga, gan.',
+      threadId: 'thread-1343',
+      commentId: 'comment-134',
+      owner: 'user-243',
     };
 
     // Action
@@ -32,5 +39,8 @@ describe('a PostReply entities', () => {
 
     // Assert
     expect(postReply.content).toEqual(payload.content);
+    expect(postReply.threadId).toEqual(payload.threadId);
+    expect(postReply.commentId).toEqual(payload.commentId);
+    expect(postReply.owner).toEqual(payload.owner);
   });
 });
