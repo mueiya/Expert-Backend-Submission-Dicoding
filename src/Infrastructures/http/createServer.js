049aa498538d56eb221098afc83f640eb1d6ab16
mueiya <mueiya.whatever@gmail.com>
2023-04-13 23:56:31 +0700

    adding jwt auth, register plugin, and server.route

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -1,15 +1,61 @@
+// Hapi
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
+
+// Error Handling
 const ClientError = require('../../Commons/exceptions/ClientError');
 const DomainErrorTranslator = require('../../Commons/exceptions/DomainErrorTranslator');
+
+// Api
 const users = require('../../Interfaces/http/api/users');
 const authentications = require('../../Interfaces/http/api/authentications');
+const threads = require('../../Interfaces/http/api/threads');
+const comments = require('../../Interfaces/http/api/comments');
+const replies = require('../../Interfaces/http/api/replies');
 
 const createServer = async (container) => {
+  // server configuration
   const server = Hapi.server({
-    host: process.env.HOST,
     port: process.env.PORT,
+    host: process.env.HOST,
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
   });
 
+  /**
+   * Hapi Jwt registration and strategy
+   * Need executed before the the protected plugin registered
+   */
+  // registration
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  // strategy
+  server.auth.strategy('forumapi_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
+  });
+
+  /**
+   * Plugin Registration
+   */
   await server.register([
     {
       plugin: users,
@@ -19,7 +65,27 @@ const createServer = async (container) => {
       plugin: authentications,
       options: {container},
     },
+    {
+      plugin: threads,
+      options: {container},
+    },
+    {
+      plugin: comments,
+      options: {container},
+    },
+    {
+      plugin: replies,
+      options: {container},
+    },
   ]);
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: () => ({
+      value: 'Hello world!',
+    }),
+  });
 
   server.ext('onPreResponse', (request, h) => {
     // mendapatkan konteks response dari request
