aea4bb1975ab7a2f63f2c4d23d0d0e46825bbbe4
mueiya <mueiya.whatever@gmail.com>
2023-04-14 11:18:22 +0700

    adding get detail handler

--- src/Interfaces/http/api/threads/handler.js
+++ src/Interfaces/http/api/threads/handler.js
@@ -1,10 +1,12 @@
 const AddThreadUseCase = require('../../../../Applications/use_case/AddThreadUseCase');
+const ViewDetailThreadByIdUseCase = require('../../../../Applications/use_case/ViewDetailThreadByIdUseCase');
 
 class ThreadsHandler {
   constructor(container) {
     this._container = container;
 
     this.postThreadHandler = this.postThreadHandler.bind(this);
+    this.getThreadHandler = this.getThreadHandler.bind(this);
   }
   async postThreadHandler(request, h) {
     const payload = request.payload;
@@ -20,11 +22,32 @@ class ThreadsHandler {
 
     const response = h.response({
       status: 'success',
+      message: 'thread posted',
       data: {
         addedThread,
       },
     });
     response.code(201);
+    return response;
+  }
+
+  async getThreadHandler(request, h) {
+    const {threadId} = request.params;
+    const useCasePayload = {
+      id: threadId,
+    };
+
+    const vievDetailThreadByIdUseCase = this._container.getInstance(ViewDetailThreadByIdUseCase.name);
+    const {thread} = await vievDetailThreadByIdUseCase.execute(useCasePayload);
+
+    const response = h.response({
+      status: 'success',
+      message: 'get thread detail success',
+      data: {
+        thread,
+      },
+    });
+    response.code(200);
     return response;
   }
 }
