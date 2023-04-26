880420b0029ef6119d18d8fddd35f3179c2b8434
mueiya <mueiya.whatever@gmail.com>
2023-04-26 18:09:20 +0700

    adding domain level commentlikerepository

---
+++ src/Domains/commentLikes/CommentLikeRepository.js
@@ -0,0 +1,13 @@
+class CommentLikeRepository {
+  async addCommentLike(commentId) {
+    throw new Error('COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+  async deleteCommentLike(commentId) {
+    throw new Error('COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+  async getCommentLike(commentId) {
+    throw new Error('COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentLikeRepository;
---
+++ src/Domains/commentLikes/_test/CommentLikeRepository.test.js
@@ -0,0 +1,13 @@
+const CommentLikeRepository = require('../CommentLikeRepository');
+
+describe('CommentLikeRepository interface', () => {
+  it('should threw error when invoke unimplemented method', async () => {
+    // Arrange
+    const commentLikeRepository = new CommentLikeRepository();
+
+    // Action & Assert
+    await expect(commentLikeRepository.addCommentLike('')).rejects.toThrowError('COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(commentLikeRepository.deleteCommentLike('')).rejects.toThrowError('COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(commentLikeRepository.getCommentLike('')).rejects.toThrowError('COMMENT_LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  });
+});
